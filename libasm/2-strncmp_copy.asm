b6f13c1a10905682b2b2e024f500ebeb2412f6a0
Nachop51 <nachoperalta0@gmail.com>
2023-03-14 22:36:24 +0000

    testing with the other version

--- libasm/2-strncmp_copy.asm
+++ libasm/2-strncmp_copy.asm
@@ -19,6 +19,9 @@ asm_strncmp:
   push r9 ; same thing
   push r10
 
+  mov r8, 0 ; Initialize both in 0
+  mov r9, 0 ;
+
   ; The first six parameters are passed through registers:
   ; ('rdi', 'rsi', 'rdx', 'rcx', 'r8', 'r9')
   ; Any additional parameter will be pushed to the stack
@@ -27,6 +30,7 @@ asm_strncmp:
   mov r10, 0 ; set r10 to 0
 
   strcmp:
+
     cmp r10, rdx
     je equal
 
@@ -34,42 +38,30 @@ asm_strncmp:
     mov r9b, byte [rsi] ; same thing here
 
     cmp r8b, 0h ; check if string ended
-    je str_ended
+    je substract
 
     cmp r9b, 0h ; check if string ended
-    je gret
+    je substract
 
     cmp r8b, r9b ; check if there is any difference
-    jne compare
+    jne substract
 
     inc r10
     inc rsi ; in case that the chars are equal,
     inc rdi ; increment both registers to continue
     jmp strcmp
 
-  str_ended:
-    cmp r9b, 0h ; if the first ended, check the second
-    je equal    ; if the second ended, the strings are equal
-    jmp lest    ; any other case is less than
-
   equal:
     mov rax, 0  ; put 0 on the rax register
     jmp end     ; return
 
-  compare:
-    cmp r8b, r9b
-    jg gret
-    jmp lest
-
-  gret:
-    mov rax, 1
-    jmp end
-
-  lest:
-    mov rax, -1
-    jmp end
+  substract:
+    xor rax, rax
+    mov eax, r8d
+    sub eax, r9d
 
   end:
+    pop r10
     pop r9 ; clean the registers before leaving
     pop r8 ; important!! in order
 

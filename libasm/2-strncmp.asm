4763b1f0444e3f34848f91a393878d1e04619f80
Nachop51 <nachoperalta0@gmail.com>
2023-03-14 21:39:29 +0000

    asm_strncmp

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,64 +1,66 @@
 BITS 64
 
-  global asm_strcmp
+  global asm_strncmp
 
   ; C Definition of the function:
   ;
-  ; int asm_strcmp(const char *s1, const char *s2);
+  ; int asm_strncmp(const char *s1, const char *s2, size_t n);
   ;
   ; To return a value use, remember to use the rax register
 
   section .text
 
-asm_strcmp:
+asm_strncmp:
   ; function prologue
   push rbp
   mov rbp, rsp
 
   push r8 ; push register in case they were used before
   push r9 ; same thing
+  push r10
+
+  mov r8, 0 ; Initialize both in 0
+  mov r9, 0 ;
+
+  ; The first six parameters are passed through registers:
+  ; ('rdi', 'rsi', 'rdx', 'rcx', 'r8', 'r9')
+  ; Any additional parameter will be pushed to the stack
+
+  ; (rdx) here is the n
+  mov r10, 0 ; set r10 to 0
 
   strcmp:
+
+    cmp r10, rdx
+    je equal
+
     mov r8b, byte [rdi] ; load a string into r8 register, and use the low bites
     mov r9b, byte [rsi] ; same thing here
 
     cmp r8b, 0h ; check if string ended
-    je str_ended
+    je substract
 
     cmp r9b, 0h ; check if string ended
-    je gret
+    je substract
 
     cmp r8b, r9b ; check if there is any difference
-    jne compare
+    jne substract
 
+    inc r10
     inc rsi ; in case that the chars are equal,
     inc rdi ; increment both registers to continue
     jmp strcmp
-
-  str_ended:
-    cmp r9b, 0h ; if the first ended, check the second
-    je equal    ; if the second ended, the strings are equal
-    jmp lest    ; any other case is less than
 
   equal:
     mov rax, 0  ; put 0 on the rax register
     jmp end     ; return
 
-  compare:
-    cmp r8b, r9b
-    jg gret
-    jmp lest
-
-  gret:
-    mov rax, 1
-    jmp end
-
-  lest:
-    mov rax, -1
-    jmp end
+  substract:
+    xor rax, rax
+    mov eax, r8d
+    sub eax, r9d
 
   end:
-
     pop r10
     pop r9 ; clean the registers before leaving
     pop r8 ; important!! in order
